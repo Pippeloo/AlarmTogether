c87571406e40326ed08f7048bad11e10c3390aff
Jules Torfs <julestorfs@amps.be>
2023-05-03 21:02:30 +0200

    Updated Homescreen

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -5,21 +5,23 @@ import {
     Button,
 } from 'react-native';
 
+import { CButton } from '../components';
+
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
 
 function HomeScreen({ navigation }: any) {
     return (
-      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#0f3057' }}>
-        <Text style={{color: '#FFD700', fontFamily: 'Party LET', fontSize: 60}}
-        >Home Screen</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => navigation.navigate('Details')}
-        />
-      </View>
+        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center', backgroundColor: '#0f3057' }}>
+            <Text style={{color: '#FFD700', fontSize: 60}}
+            >Home Screen</Text>
+            {/* Set view at the bottom */}
+            <View style={{position: 'absolute', bottom: 20, width: '90%'}}>
+                <CButton text="Go to Details" onPress={() => navigation.navigate('Details')}/>
+            </View>
+        </View>
     );
-  }
+}
 
 export default HomeScreen;
